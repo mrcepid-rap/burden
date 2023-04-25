d181adfedde81e6ef720830e623438489c9d6377
ejgardner_adrestia <ejgardner@adrestia.com>
2023-04-25 09:30:30 +0100

    Modernised setup.py and requirements.txt

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from pathlib import Path
+
+
+def load_requirements(fname: Path):
+    reqs = []
+    with fname.open('r') as reqs_file:
+        for line in reqs_file:
+            reqs.append(line.rstrip())
+    return reqs
+
 
 setup(
     name='burden',
     version='1.1.1',
-    packages=['burden', 'burden.tool_runners'],
-    url='',
+    packages=find_packages(),
+    url='https://github.com/mrcepid-rap/mrcepid-runassociationtesting-burden',
     license='',
-    author='ejgardner',
-    author_email='',
+    author='Eugene Gardner',
+    author_email='eugene.gardner@mrc-epid.cam.ac.uk',
     description='',
-    install_requires=['pandas>1.4', 'dxpy>0.326.1'],
-    python_requires='>=3'
+    install_requires=load_requirements(Path('requirements.txt'))
 )
